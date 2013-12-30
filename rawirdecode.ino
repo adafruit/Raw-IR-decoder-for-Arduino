70f2870a70adf211c75f1e072841825a5cb07124
ToniA <ToniA@server.fake>
2013-12-30 16:54:26 +0200

    Mitsubishi EF maintenance mode, Carrier codes

--- rawirdecode.ino
+++ rawirdecode.ino
@@ -16,8 +16,14 @@
 //uint8_t IRpin = 2;
 // Digital pin #2 is the same as Pin D2 see
 // http://arduino.cc/en/Hacking/PinMapping168 for the 'raw' pin mapping
+
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+#define IRpin_PIN      PINE
+#define IRpin          4
+#else
 #define IRpin_PIN      PIND
 #define IRpin          2
+#endif
 
 // the maximum pulse we'll listen for - 65 milliseconds is a long time
 #define MAXPULSE 65000
@@ -27,6 +33,17 @@
 // accurate timing
 uint16_t RESOLUTION=20;
 
+
+#define BYTETOBINARYPATTERN "%d%d%d%d%d%d%d%d"
+#define BYTETOBINARY(byte)  \
+  (byte & 0x80 ? 1 : 0), \
+  (byte & 0x40 ? 1 : 0), \
+  (byte & 0x20 ? 1 : 0), \
+  (byte & 0x10 ? 1 : 0), \
+  (byte & 0x08 ? 1 : 0), \
+  (byte & 0x04 ? 1 : 0), \
+  (byte & 0x02 ? 1 : 0), \
+  (byte & 0x01 ? 1 : 0)
 
 // The thresholds for different symbols
 // These should work with Panasonic (from DKE on), Fujitsu and Mitsubishi
@@ -251,9 +268,9 @@ void printpulses(void) {
   }
   Serial.println();
 
-  // If this looks like a Mitsubishi FD-25 code...
+  // If this looks like a Mitsubishi FD-25 or FE code...
   if ( byteCount == 36 && bytes[0] == 0x23 && (memcmp(bytes, bytes+18, 17) == 0)) {
-    Serial.println("Looks like a Mitsubishi FD-25 protocol");
+    Serial.println("Looks like a Mitsubishi FD / FE series protocol");
 
     // Check if the checksum matches
     byte checksum = 0;
@@ -283,11 +300,18 @@ void printpulses(void) {
 
     // Operating mode
     switch (bytes[6] & 0x38) { // 0b00111000
+      case 0x38:
+        Serial.println("MODE FAN");
+        break;
       case 0x20:
         Serial.println("MODE AUTO");
         break;
       case 0x08:
-        Serial.println("MODE HEAT");
+        if (bytes[15] == 0x20) {
+          Serial.println("MODE MAINTENANCE HEAT (FE only)");
+        } else {
+          Serial.println("MODE HEAT");
+        }
         break;
       case 0x18:
         Serial.println("MODE COOL");
@@ -322,7 +346,11 @@ void printpulses(void) {
 
     // Temperature
     Serial.print("Temperature: ");
-    Serial.println(bytes[7] + 16);
+    if (bytes[7] == 0x00) {
+      Serial.println("10");
+    } else {
+      Serial.println(bytes[7] + 16);
+    }
 
     // Fan speed
     switch (bytes[9] & 0x07) { // 0b00000111
@@ -405,4 +433,132 @@ void printpulses(void) {
         break;
     }
   }
+
+  // If this looks like a Carrier code...
+  if ( byteCount == 18 && bytes[0] == 0x4F && bytes[1] == 0xB0 && (memcmp(bytes, bytes+9, 9) == 0)) {
+    Serial.println("Looks like a Carrier protocol");
+
+    // Check if the checksum matches
+    byte checksum = 0;
+
+    checksum = bitReverse(bytes[0]) +
+               bitReverse(bytes[1]) +
+               bitReverse(bytes[2]) +
+               bitReverse(bytes[3]) +
+               bitReverse(bytes[4]) +
+               bitReverse(bytes[5]) +
+               bitReverse(bytes[6]) +
+               bitReverse(bytes[7]);
+
+    switch (bytes[6] & 0x0F) {
+      case 0x00:
+        Serial.println("FAN: AUTO");
+        break;
+      case 0x02:
+        Serial.println("FAN: 1");
+        break;
+      case 0x06:
+        Serial.println("FAN: 2");
+        break;
+      case 0x01:
+        Serial.println("FAN: 3");
+        break;
+      case 0x05:
+        Serial.println("FAN: 4");
+        break;
+      case 0x03:
+        Serial.println("FAN: 5");
+        break;
+    }
+
+    switch (bytes[6] & 0xF0) {
+      case 0xE0:
+        Serial.println("MODE: OFF");
+        break;
+      case 0x00:
+        Serial.println("MODE: AUTO");
+        checksum += 0x02;
+        switch (bytes[6] & 0x0F) {
+          case 0x02: // FAN1
+          case 0x03: // FAN5
+          case 0x06: // FAN2
+            checksum += 0x80;
+            break;
+        }
+        break;
+      case 0x80:
+        Serial.println("MODE: COOL");
+        break;
+      case 0x40:
+        Serial.println("MODE: DRY");
+        checksum += 0x02;
+        break;
+      case 0xC0:
+        Serial.println("MODE: HEAT");
+        switch (bytes[6] & 0x0F) {
+          case 0x05: // FAN4
+          case 0x06: // FAN2
+            checksum += 0xC0;
+            break;
+        }
+        break;
+      case 0x20:
+        Serial.println("MODE: FAN");
+        checksum += 0x02;
+        switch (bytes[6] & 0x0F) {
+          case 0x02: // FAN1
+          case 0x03: // FAN5
+          case 0x06: // FAN2
+            checksum += 0x80;
+            break;
+        }
+        break;
+    }
+
+    checksum = bitReverse(checksum);
+
+    const byte temperatures[]  = { 17, 25, 21, 29, 19, 27, 23, 00, 18, 26, 22, 30, 20, 28, 24 };
+
+
+    Serial.print("Temperature: ");
+    Serial.println(temperatures[bytes[5]]);
+
+    char bin1[9];
+    char bin2[9];
+    char bin3[9];
+
+    snprintf(bin1, sizeof(bin1), BYTETOBINARYPATTERN, BYTETOBINARY(checksum));
+    snprintf(bin2, sizeof(bin2), BYTETOBINARYPATTERN, BYTETOBINARY(bytes[8]));
+    snprintf(bin3, sizeof(bin3), BYTETOBINARYPATTERN, BYTETOBINARY(bytes[6]));
+
+
+    Serial.print("ModeFan  ");
+    Serial.println(bin3);
+
+
+    Serial.print("Checksum ");
+    Serial.print(bin1);
+
+    if (checksum == bytes[8]) {
+      Serial.println(" matches");
+    } else {
+      Serial.println(" does not match real");
+      Serial.print("checksum ");
+      Serial.println(bin2);
+    }
+
+  }
+}
+
+
+
+byte bitReverse(byte x)
+{
+  //          01010101  |         10101010
+  x = ((x >> 1) & 0x55) | ((x << 1) & 0xaa);
+  //          00110011  |         11001100
+  x = ((x >> 2) & 0x33) | ((x << 2) & 0xcc);
+  //          00001111  |         11110000
+  x = ((x >> 4) & 0x0f) | ((x << 4) & 0xf0);
+  return x;
 }
