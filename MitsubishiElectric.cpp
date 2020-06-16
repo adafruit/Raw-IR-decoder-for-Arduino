353eb9fcce5f56dd25dfb2ec08c4211d0e72916d
Panayotis Kouvarakis <pkouvarakis@precisionlab.gr>
2020-06-16 19:29:22 +0300

    Add support for Mitsubishi FD-50VF and RC KM09D/0096695 additional options

--- MitsubishiElectric.cpp
+++ MitsubishiElectric.cpp
@@ -35,48 +35,63 @@ bool decodeMitsubishiElectric(byte *bytes, int byteCount)
     }
 
     // Operating mode
+    Serial.print(F("MODE "));
     switch (bytes[6] & 0x38) { // 0b00111000
       case 0x38:
-        Serial.println(F("MODE FAN"));
+        Serial.println(F("FAN"));
         break;
       case 0x20:
-        Serial.println(F("MODE AUTO"));
+        Serial.println(F("AUTO"));
         break;
       case 0x08:
         if (bytes[15] == 0x20) {
-          Serial.println(F("MODE MAINTENANCE HEAT (FE only)"));
+          Serial.println(F("MAINTENANCE HEAT (FE only)"));
         } else {
-          Serial.println(F("MODE HEAT"));
+          Serial.println(F("HEAT"));
         }
         break;
       case 0x18:
-        Serial.println(F("MODE COOL"));
+        Serial.println(F("COOL"));
         break;
       case 0x10:
-        Serial.println(F("MODE DRY"));
+        Serial.println(F("DRY"));
         break;
       default:
-        Serial.println(F("MODE unknown"));
+        Serial.println(F("unknown"));
         break;
     }
 
     // I-See
+    Serial.print(F("I-See: "));
     switch (bytes[6] & 0x40) { // 0b01000000
       case 0x40:
-        Serial.println(F("I-See: ON"));
+        Serial.println(F("ON"));
         break;
       case 0x00:
-        Serial.println(F("I-See: OFF"));
+        Serial.println(F("OFF"));
         break;
     }
 
-    // Plasma
-    switch (bytes[15] & 0x40) { // 0b01000000
-      case 0x40:
-        Serial.println(F("Plasma: ON"));
+    // Clean
+    Serial.print(F("Clean: "));
+    switch (bytes[14] & 0x04) { // 0b00000100
+      case 0x04:
+        Serial.println(F("ON"));
         break;
       case 0x00:
-        Serial.println(F("Plasma: OFF"));
+        Serial.println(F("OFF"));
+        break;
+    }
+
+
+    // Plasma
+    Serial.print(F("Plasma: "));
+    switch (bytes[15] & 0x40) { // 0b01000000
+      case 0x40:
+        Serial.println(F("ON"));
+        break;
+      case 0x00:
+        Serial.println(F("OFF"));
         break;
     }
 
@@ -89,86 +104,127 @@ bool decodeMitsubishiElectric(byte *bytes, int byteCount)
     }
 
     // Fan speed
+    Serial.print(F("FAN "));
     switch (bytes[9] & 0x07) { // 0b00000111
       case 0x00:
-        Serial.println(F("FAN AUTO"));
+        Serial.println(F("AUTO"));
         break;
       case 0x01:
-        Serial.println(F("FAN 1"));
+        Serial.println(F("1"));
         break;
       case 0x02:
-        Serial.println(F("FAN 2"));
+        Serial.println(F("2"));
         break;
       case 0x03:
-        Serial.println(F("FAN 3"));
+        Serial.println(F("3"));
         break;
       case 0x04:
-        Serial.println(F("FAN 4"));
+        Serial.println(F("4"));
         break;
       default:
-        Serial.println(F("FAN unknown"));
+        Serial.println(F("unknown"));
         break;
     }
 
     // Vertical air direction
+    Serial.print(F("VANE: "));
     switch (bytes[9] & 0xF8) { // 0b11111000
       case 0x40: // 0b01000
-        Serial.println(F("VANE: AUTO1?"));
+        Serial.println(F("AUTO1?"));
         break;
       case 0x48: // 0b01001
-        Serial.println(F("VANE: UP"));
+        Serial.println(F("UP"));
         break;
       case 0x50: // 0b01010
-        Serial.println(F("VANE: UP-1"));
+        Serial.println(F("UP-1"));
         break;
       case 0x58: // 0b01011
-        Serial.println(F("VANE: UP-2"));
+        Serial.println(F("UP-2"));
         break;
       case 0x60: // 0b01100
-        Serial.println(F("VANE: UP-3"));
+        Serial.println(F("UP-3"));
         break;
       case 0x68: // 0b01101
-        Serial.println(F("VANE: DOWN"));
+        Serial.println(F("DOWN"));
         break;
       case 0x78: // 0b01111
-        Serial.println(F("VANE: SWING"));
+        Serial.println(F("SWING"));
         break;
       case 0x80: // 0b10000
-        Serial.println(F("VANE: AUTO2?"));
+        Serial.println(F("AUTO2?"));
         break;
       case 0xB8: // 0b10111
-        Serial.println(F("VANE: AUTO3?"));
+        Serial.println(F("AUTO3?"));
         break;
       default:
-        Serial.println(F("VANE: unknown"));
+        Serial.println(F("unknown"));
         break;
     }
 
     // Horizontal air direction
+    Serial.print(F("WIDE VANE: "));
     switch (bytes[8] & 0xF0) { // 0b11110000
+      case 0x00:
+        Serial.println(F("AREA"));
+        break;
       case 0x10:
-        Serial.println(F("WIDE VANE: LEFT"));
+        Serial.println(F("LEFT"));
         break;
       case 0x20:
-        Serial.println(F("WIDE VANE: MIDDLE LEFT"));
+        Serial.println(F("MIDDLE LEFT"));
         break;
       case 0x30:
-        Serial.println(F("WIDE VANE: MIDDLE"));
+        Serial.println(F("MIDDLE"));
         break;
       case 0x40:
-        Serial.println(F("WIDE VANE: MIDDLE RIGHT"));
+        Serial.println(F("MIDDLE RIGHT"));
         break;
       case 0x50:
-        Serial.println(F("WIDE VANE: RIGHT"));
+        Serial.println(F("RIGHT"));
         break;
       case 0xC0:
-        Serial.println(F("WIDE VANE: SWING"));
+        Serial.println(F("SWING"));
         break;
       default:
-        Serial.println(F("WIDE VANE: unknown"));
+        Serial.println(F("unknown"));
         break;
     }
 
+    // Horizontal air direction, area mode
+    Serial.print(F("AREA MODE: "));
+    switch (bytes[13] & 0xC0) { // 0b11000000
+      case 0x00:
+        Serial.println(F("OFF"));
+        break;
+      case 0x40:
+        Serial.println(F("LEFT"));
+        break;
+      case 0x80:
+        Serial.println(F("AUTO"));
+        break;
+      case 0xC0:
+        Serial.println(F("RIGHT"));
+        break;
+    }
+
+    // Installation position
+    Serial.print(F("INSTALL POSITION: "));
+    switch (bytes[14] & 0x18) { // 0b00011000
+      case 0x08:
+        Serial.println(F("LEFT"));
+        break;
+      case 0x10:
+        Serial.println(F("CENTER"));
+        break;
+      case 0x18:
+        Serial.println(F("RIGHT"));
+        break;
+      default:
+        Serial.println(F("unknown"));
+        break;
+    }
+
+    
     return true;
   }
 
